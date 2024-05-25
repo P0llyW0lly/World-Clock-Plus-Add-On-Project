8a1d70bfb52e40f603d160a71ba8f8695334922d
P0llyW0lly <paula.h33nan@gmail.com>
2024-05-25 10:01:48 +0100

    Added JS for select

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ function updateTime() {
             "h:mm:ss [<small>]A[</small>]"
         );
     }
-
     let kyivElement = document.querySelector("#kyiv");
     if (kyivElement) {
         let kyivDateElement = kyivElement.querySelector(".date");
@@ -23,10 +22,6 @@ function updateTime() {
         );
     }
 }
-
-updateTime();
-setInterval(updateTime, 1000);
-
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
@@ -35,8 +30,7 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-  <div class="city">
+  citiesElement.innerHTML =`<div class="city">
     <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
